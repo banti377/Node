c13dce3c20403365d43d555869a14943a684a6f0
Banti Patel <bantipatel2506@gmail.com>
2024-03-07 14:10:53 +0530

    add data

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import bodyParser from "body-parser";
+import Express from "express";
+import { connectDB } from "./db";
+import userRouter from "./router/user";
+
+const app = Express();
+
+const port = process.env.PORT || 3000;
+
+app.use(Express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/user", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("------>");
+});
+
+app.listen(port, () => {
+  connectDB();
+  console.log(`Server is running on http://localhost:${port}`);
+});
